8b776be9d96d61e355cbb0bbad9d76ef6073878e
Ivan Barchuk <vandred@ukr.net>
2018-11-15 11:24:30 +0200

    add unit trst

---
+++ src/backEnd/BungalowCore/XUnitTestProject1/BookingControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Bungalow.Web.Host.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    
+    public class BookingControllerTests
+    {
+        [Fact]
+        public void SetUpBaseFeeTest() {
+           // BookingController controler = new BookingController();
+
+        }
+    }
+}
